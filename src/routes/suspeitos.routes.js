712613b96b3c7ca3fdefc85d9ab54f7b314fe0be
Carlos Eduardo <cadu.augusto10@gmail.com>
2024-10-10 11:51:30 -0300

    feat: finish project

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -44,4 +44,117 @@ let suspeitos = [
 suspeitosRoutes.get("/", (req, res) => {
   return res.status(200).json(suspeitos);
 });
-  export default suspeitosRoutes
+
+// Rota para cadastrar um novo suspeito
+suspeitosRoutes.post("/", (req, res) => {
+  const { nome, idade, descricao, envolvimento } = req.body;
+
+  console.log(Number,isInteger(envolvimento));
+  
+
+  // Validação dos campos obrigatórios
+  if (!nome || !idade || !envolvimento) {
+    return res.status(400).json({
+      message: "Os campos nome, idade e água são obrigatórios!",
+    });
+  }
+
+  // Validação de existência de Envolvimento
+  if (envolvimento != "sim" && envolvimento != "não") {
+    return res.status(400).send({
+      message: "Digite 'sim' ou 'não'!",
+    });
+  }
+
+  // Criação de um novo suspeito
+  const novoSuspeito = {
+    id: Math.floor(Math.random() * 1000000),
+    nome,
+    idade,
+    descricao,
+    envolvimento
+  };
+
+  // Adiciona o novo suspeito ao array de Suspeitos
+  suspeitos.push(novoSuspeito);
+
+  return res.status(201).json({
+    message: "suspeito cadastrado com sucesso!",
+    novoSuspeito,
+  });
+});
+
+// Rota para buscar um suspeito pelo id
+suspeitosRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  // Busca um suspeito pelo id no array de suspeitos
+  const suspeito = suspeitos.find((suspect) => suspect.id == id);
+
+  // Verifica se o suspeito foi encontrado
+  if (!suspeito) {
+    return res
+      .status(404)
+      .json({ message: `suspeito com id ${id} não encontrado!` });
+  }
+
+  return res.status(200).json(suspeito);
+});
+
+// Rota para atualizar um suspeito pelo id
+suspeitosRoutes.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { nome, idade, descricao, envolvimento } = req.body;
+
+  // Busca um suspeito pelo id no array de suspeitos
+  const suspeito = suspeitos.find((p) => p.id == id);
+
+  // Validação dos campos obrigatórios
+  if (!nome || !idade || !envolvimento) {
+    return res.status(400).json({
+      message: "Os campos nome, idade e envolvimento são obrigatórios!",
+    });
+  }
+
+  // Validação de existência de envolvimento
+  if (envolvimento != "sim" && envolvimento != "não") {
+    return res.status(400).send({
+      message: "Digite 'sim' ou 'não'!",
+    });
+  }
+
+  suspeito.nome = nome;
+  suspeito.idade = idade;
+  suspeito.descricao = descricao;
+  suspeito.envolvimento = envolvimento ;
+
+  return res.status(200).json({
+    message: "suspeito atualizado com sucesso!",
+    suspeito,
+  });
+});
+
+// Rota para deletar um suspeito
+suspeitosRoutes.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  // Busca um suspeito pelo id no array de suspeitos
+  const suspeito = suspeitos.find((suspect) => suspect.id == id);
+
+  // Verifica se o suspeito foi encontrado
+  if (!suspeito) {
+    return res
+      .status(404)
+      .json({ message: `suspeito com id ${id} não encontrado!` });
+  }
+
+  // Remove o suspeito do array de suspeitos
+  suspeitos = suspeitos.filter((suspect) => suspect.id != id);
+
+  return res.status(200).json({
+    message: "suspeito removido com sucesso!",
+    suspeito,
+  });
+});
+
+export default suspeitosRoutes;
